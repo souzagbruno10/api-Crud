912345f1c6cf9e596e47bb642d54d7ca624bbdf8
Bruno Gomes de Souza <souzagbruno10@gmail.com>
2025-02-06 15:44:53 -0300

    crud

---
+++ src/test/java/com/example/api_Crud/api_Crud/ApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_Crud.api_Crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
