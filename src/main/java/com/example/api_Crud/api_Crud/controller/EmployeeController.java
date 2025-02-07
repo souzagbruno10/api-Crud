b314a222c7fd5cd198d4f0a3a3cd3c002af10014
Bruno Gomes de Souza <souzagbruno10@gmail.com>
2025-02-07 09:32:32 -0300

    modified some existing files

--- src/main/java/com/example/api_Crud/api_Crud/controller/EmployeeController.java
+++ src/main/java/com/example/api_Crud/api_Crud/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.api_Crud.api_Crud.controller;
 
+import com.example.api_Crud.api_Crud.model.Employee;
 import com.example.api_Crud.api_Crud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +21,22 @@ public class EmployeeController {
     public String viewHomePage(Model model){
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model ){
+        //create model attribute to bind form data
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee")Employee employee){
+        //save employee to database
+        employeeService.saveEmployee(employee);
+        return"redirect:/";
+
     }
 }
--- src/main/java/com/example/api_Crud/api_Crud/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/api_Crud/api_Crud/service/EmployeeServiceImpl.java
@@ -17,4 +17,10 @@ public class EmployeeServiceImpl implements EmployeeService{
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+
+    }
 }
